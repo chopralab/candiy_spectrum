0d31dcc5f1c50757867d3caf348f2ae941570d98
anandrajasekar <anand.rajasekar18@gmail.com>
2020-08-12 18:40:45 +0530

    [feat] Add function to scrap InChI key of molecule
    InChI key is obtained for molecules with CAS IDs in species.txt file.

--- scrap.py
+++ scrap.py
@@ -1,5 +1,4 @@
 import os
-from bs4 import BeautifulSoup as bs
 import requests, urllib
 import argparse
 import logging
@@ -7,6 +6,7 @@ import pandas as pd
 
 from model.utils import set_logger
 
+nist_url = "https://webbook.nist.gov/cgi/cbook.cgi"
 
 def scrap_data(cas_ls, params, data_dir):
 	'''Collect data from NIST database and store them in jdx format.
@@ -18,8 +18,7 @@ def scrap_data(cas_ls, params, data_dir):
 
     Returns:
         None
-    '''
-	nist_url = "https://webbook.nist.gov/cgi/cbook.cgi"
+    '''	
 
 	#Create directory for the relevant spetra 
 	spectra_path = os.path.join(data_dir, params['Type'].lower(), '')
@@ -37,25 +36,58 @@ def scrap_data(cas_ls, params, data_dir):
 		logging.info('Creating {} spectra for id: {}. Total spectra created {}'.format(params['Type'].lower(), cas_id, num_created))
 		with open(spectra_path +cas_id +'.jdx', 'wb') as data:
 			data.write(response.content)
+
+def scrap_inchi(cas_ls, params, data_dir):
+	'''Collect Inchi keys from NIST database and store them in txt format.
+
+    Args:
+        cas_ls: (list) CAS ids to download data for
+		params: (dict) queries to be added to url
+		data_dir: (string) path to store the data
+
+    Returns:
+        None
+    '''	
+
+	#Create file path for storing inchi keys
+	inchi_path = os.path.join(data_dir, 'inchi.txt')
+	num_created = 0
+	with open(inchi_path,'a') as file:
+		content = '{}\t{}\n'.format('cas_id', 'inchi')
+		file.write(content)
+
+		for cas_id in cas_ls:
+			params['GetInChI'] = 'C' + cas_id
+			response = requests.get(nist_url, params=params)
+
+			num_created+=1
+			logging.info('Creating InChi key for id: {}. Total keys created {}'.format(cas_id, num_created))
+			content = '{}\t{}\n'.format(cas_id,response.content.decode("utf-8"))
+			file.write(content)
 			
 	
 
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', default= './data',\
+parser.add_argument('--save_dir', default= './data',\
      help = "Directory path to store scrapped data")
-parser.add_argument('--cas_smiles_list', default= 'species.txt',\
-    help = "File containing CAS number and smiles of molecules")
+parser.add_argument('--cas_list', default= 'species.txt',\
+    help = "File containing CAS number and formula of molecules")
 parser.add_argument('--scrap_IR', default= True,\
     help = "Whether to download IR or not")
 parser.add_argument('--scrap_MS', default= True,\
     help = "Whether to download MS or not")
+parser.add_argument('--scrap_InChi', default= True,\
+    help = "Whether to download InChi or not")
 
 args = parser.parse_args()
 
+#Check if file containing CAS ids exist
+assert os.path.isfile(args.cas_list),"No file named {} exists".format(args.cas_list)
+
 #Create data directory to store logs and spectra
-data_dir = args.data_dir
+data_dir = args.save_dir
 if not os.path.exists(data_dir):
 	os.makedirs(data_dir)
 
@@ -63,11 +95,11 @@ set_logger(data_dir, 'scrap.log')
 
 #Obtain CAS ids used for downloading the content from NIST
 logging.info('Loading CAS file')
-cas_smiles_df = pd.read_csv(args.cas_smiles_list, sep='\t', names = ['name', 'smiles', 'cas'], header = 0)
-cas_smiles_df.dropna(subset=['cas'], inplace=True)
-cas_smiles_df.cas = cas_smiles_df.cas.str.replace('-', '')
+cas_df = pd.read_csv(args.cas_list, sep='\t', names = ['name', 'formula', 'cas'], header = 0)
+cas_df.dropna(subset=['cas'], inplace=True)
+cas_df.cas = cas_df.cas.str.replace('-', '')
 
-cas_ids = list(cas_smiles_df.cas)
+cas_ids = list(cas_df.cas)
 
 
 
@@ -81,3 +113,8 @@ logging.info('Scrap IR spectra')
 if args.scrap_IR:
 	params={'JCAMP': '', 'Type': 'IR', 'Index': 0}	
 	scrap_data(cas_ids, params, data_dir)
+
+logging.info('Scrap InChi keys')
+if args.scrap_InChi:
+	params={}
+	scrap_inchi(cas_ids, params, data_dir)
